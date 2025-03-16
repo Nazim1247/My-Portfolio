cd479bb05a42ff7d01556b3e57b655299ba44686
nazim uddin <mdnajim1247@gmail.com>
2025-03-16 19:48:58 +0600

    updated code

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,6 +11,19 @@ const Projects = () => {
             <div className='mt-8'>
             <h2 className='text-3xl font-bold text-center py-4'>My Projects</h2>
             <div className='flex flex-col lg:flex-row items-center gap-6 justify-center'>
+
+            <div className='space-y-4 bg-gray-200 dark:bg-gray-800 shadow p-4 rounded-lg hover:scale-110 transition'>
+            <h2 className='text-xl font-bold text-center'>Dream House</h2>
+
+            <a href="https://assignment-12-2f479.web.app"><img title='Click to view live site' className='rounded-lg' src={project3} alt="" /></a>
+
+            <div className='flex justify-center items-center gap-4'>
+            <p className=''><Link to='/project3Details' className='btn btn-sm btn-primary' href="">Details</Link></p>
+            <a href="https://assignment-12-2f479.web.app"><button className='btn btn-sm btn-primary'>Live Link</button></a>
+            <a href="https://github.com/Nazim1247/Assignment-12-client"><button className='btn btn-sm btn-primary'>Github Link</button></a>
+            </div>
+            </div>
+            
             <div className='space-y-4 bg-gray-200 dark:bg-gray-800 shadow p-4 rounded-lg hover:scale-110 transition'>
             <h2 className='text-xl font-bold text-center'>Online Tutor Booking Platform</h2>
 
@@ -33,17 +46,7 @@ const Projects = () => {
             <a href="https://github.com/Nazim1247/Assignment-10-client"><button className='btn btn-sm btn-primary'>Github Link</button></a>
             </div>
             </div>
-            <div className='space-y-4 bg-gray-200 dark:bg-gray-800 shadow p-4 rounded-lg hover:scale-110 transition'>
-            <h2 className='text-xl font-bold text-center'>Dream House</h2>
-
-            <a href="https://assignment-12-2f479.web.app"><img title='Click to view live site' className='rounded-lg' src={project3} alt="" /></a>
-
-            <div className='flex justify-center items-center gap-4'>
-            <p className=''><Link to='/project3Details' className='btn btn-sm btn-primary' href="">Details</Link></p>
-            <a href="https://assignment-12-2f479.web.app"><button className='btn btn-sm btn-primary'>Live Link</button></a>
-            <a href="https://github.com/Nazim1247/Assignment-12-client"><button className='btn btn-sm btn-primary'>Github Link</button></a>
-            </div>
-            </div>
+            
         </div>
         </div>
         </Zoom>
