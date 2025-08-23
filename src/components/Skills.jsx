c46330f83b98491302666ec36e612ae5e0abea11
nazim uddin <mdnajim1247@gmail.com>
2025-08-23 06:05:21 +0600

    change style

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -24,16 +24,16 @@ const Skills = () => {
     cssEase: "linear",
     responsive: [
       {
-        breakpoint: 600,
-        settings: { slidesToShow: 1, slidesToScroll: 1, }
+        breakpoint: 430,
+        settings: { slidesToShow: 1 }
       },
       {
-        breakpoint: 768,
-        settings: { slidesToShow: 2, slidesToScroll: 1, }
+        breakpoint: 640,
+        settings: { slidesToShow: 2 }
       },
       {
         breakpoint: 1024,
-        settings: { slidesToShow: 3, slidesToScroll: 1, }
+        settings: { slidesToShow: 3 }
       },
     ]
   };
