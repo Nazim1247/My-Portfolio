f25f92e76a3e1a8bb4c4916fbca0a06649fc5fda
nazim uddin <mdnajim1247@gmail.com>
2025-08-23 08:10:41 +0600

    agian update

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -25,17 +25,18 @@ const Skills = () => {
     cssEase: "linear",
     responsive: [
       {
-        breakpoint: 480,
-        settings: { slidesToShow: 1 }
+        breakpoint: 1024,
+        settings: { slidesToShow: 3 }
       },
       {
         breakpoint: 640,
         settings: { slidesToShow: 2 }
       },
       {
-        breakpoint: 1024,
-        settings: { slidesToShow: 3 }
+        breakpoint: 480,
+        settings: { slidesToShow: 1 }
       },
+      
     ]
   };
     return (
